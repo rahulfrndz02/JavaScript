48bd7458542f228cbc2ad8cd7165f70d23eee043
Rahul Raj <rahulfrndz02@gmail.com>
2023-06-14 14:37:21 +0530

    first commit

---
+++ Intro/functions.js
@@ -0,0 +1,89 @@
+/*
+
+access_modifier returntype name(parameters) {
+    // code 
+}
+
+javascript
+
+function name(parameters){
+    // code
+}
+*/
+
+// class Test {
+//     protected Integer callme(int a , Integer b){
+//         // code
+
+//         return 10; 
+//     }
+// }
+
+// let x = 20 ;
+// let z = x ;
+
+// class Test {
+//     #callme(){
+//         console.log("callme")
+//     }
+//     test(){
+//         this.#callme();
+//         console.log("Inside test")
+//     }
+// }
+
+// let t = new Test();
+// t.test();
+
+// console.log(typeof x, typeof z) // number
+// function callme(a, b){
+//    let result = a + b ;
+// }
+
+// let y = callme ;
+
+// console.log(typeof y)
+// let x = 20; // number data assignment
+// let func = function(a, b, c) {
+//     // let a = 10, b = 20, c ;
+//     console.log(a + b + c)// 10 + 20 + undefined
+//     // a = 10, b = 20, c = 30
+//     // console.log(a + b + c); // 10 + 20 + 30 = 60
+//     // return "aravind"; 
+//     // return undefined ;
+// }
+// let result = func(10, 20)
+// console.log(result) // false
+
+/*
+    + => arithmatic
+    + => concatination operator
+*/
+
+/*
+    2 + (3 + 'abc')
+    2 + "3abc"
+    "23abc"
+
+    "abc" + 2 + 3
+
+    "abc2" + 3
+
+    "abc23"
+*/
+
+
+// let x = 2 + '5' => 25
+// let x = 2 - "5" // 2 - Number("5") => 2 - 5 => -3
+
+// let y = 65 - "a" // 65 - Number("a") => 65 - NaN => NaN
+
+// let x = function(a, b){
+//     console.log(a + b)
+//     return true ;
+// }
+
+// let y = (a, b) =>  {
+//     console.log(a + b )
+//     return true;
+// }
---
+++ object.js
@@ -0,0 +1,88 @@
+// let user = {
+//     email:"rahul@gmail.com",
+//     password: "12345"
+// }
+// let a =234;
+// console.log(typeof a);
+
+// function a(){
+//     console.log("inside a");
+
+//     function b(){
+//         console.log("inside b");
+//     }
+// }
+
+// b();
+
+// function a(a,b,c){
+//     console.log(a+b*c);
+
+    
+// }
+
+// a(2,3);
+
+
+// function a(){
+//     console.log("inside a");
+
+//     function b(){
+//         console.log("inside b");
+//     }
+// }
+
+// a();
+
+// const lookup = {
+//     A: "N",
+//     B: "O",
+//     C: "P",
+//     D: "Q",
+//     E: "R",
+//     F: "S",
+//     G: "T",
+//     H: "U",
+//     I: "V",
+//     J: "W",
+//     K: "X",
+//     L: "Y",
+//     M: "Z",
+//     N: "A",
+//     O: "B",
+//     P: "C",
+//     Q: "D",
+//     R: "E",
+//     S: "F",
+//     T: "G",
+//     U: "H",
+//     V: "I",
+//     W: "J",
+//     X: "K",
+//     Y: "L",
+//     Z: "M",
+//     "?": "?",
+//     ",": ",",
+//   };
+  
+//   function rot13(encodedStr) {
+//     let decodedArr = []; // Your Result goes here
+//     // Only change code below this line
+//       for(let i=0; i<str.length; i++){
+//           if(lookup[str[i]] == undefined){
+//               decodedArr.push(str[i]);
+//           }
+//           else
+//               decodedArr.push(lookup[str[i]]);
+//       }
+  
+//     return decodedArr.join(" "); //return decodedArr
+//   }
+  
+  // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+  
+  // console.log(rot13("SERR YBIR? NPPVBWBO"));
+  
+  // Do not change this line
+  // window.rot13 = rot13;
+
